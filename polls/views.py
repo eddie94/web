fbb4e6c2deddda39a23537a5a9ed566557f2d9a6
eddie94 <myeddie@naver.com>
2020-03-23 13:51:10 +0900

    checklist uploaded

--- polls/views.py
+++ polls/views.py
@@ -19,25 +19,26 @@ def finished_en(request):
 def Location_en_check(request):
 
     data={
-        'locations' : ["kitchen","bedroom","living_room","bath_room","dining_room","none"],
+        'locations' : ["Kitchen","Bedroom","Livingroom","Bathroom","Diningroom","Personal_room","Hallway"],
         'things' : ["oven","microwave","toaster","sink","refrigerator",
                     "TV","laptop","remote","mouse","keyboard",
-                    "cell phone","couch","chair","potted plant","bed",
-                    "dining table","toilet","bottle","wine glass","cup",
+                    "phone","couch","chair","plant","bed",
+                    "table","toilet","bottle","glass","cup",
                     "fork","knife","spoon","bowl","book",
-                    "clock","vase","scissors","teddy bear","hair dryer",
-                    "toothbrush"]
+                    "clock","vase","scissors","bear","dryer",
+                    "toothbrush","none"]
     }
     
     if request.POST:
         num_of_files = len(os.listdir("./collected_data/"))
-        with open("./collected_data/"+str(num_of_files)+".txt",'w') as f:
-            for item in data['things']:
-                if request.POST.getlist(item):
-                    f.write(item+":")
-                    for locations in request.POST.getlist(item):
-                        f.write(locations+" ")
+        with open("./collected_data/"+str(num_of_files+1)+".txt",'w') as f:
+            for location in data['locations']:
+                if request.POST.getlist(location):
+                    f.write(location+":")
+                    for items in request.POST.getlist(location):
+                        f.write(items+" ")
                     f.write("\n")
+        return redirect("../finished/")
 
     return render(request, 'Location_en_check.html')
 
@@ -56,7 +57,7 @@ def Location_en(request):
         if os.listdir('./collected_data'):
             filename=str(len(os.listdir('./collected_data')))
             
-        with open('./collected_data/'+str(filename)+".txt", 'w') as f:
+        with open('./collected_data/'+str(filename)+"_en.txt", 'w') as f:
             for location,thing in zip(data['locations'], request.POST.getlist('things')):
                 f.write(location)
                 f.write(":")
